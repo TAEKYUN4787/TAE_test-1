7e39f7fa75a2c523d54edaecaac4b19b6042b88c
TAEKYUN <cjswo4787@gmail.com>
2023-01-26 14:43:29 +0900

    first commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import numpy as np
+import os
+
+currentpath = os.getcwd()
+A = pd.read_csv("abc12.csv")
+#A = pd.read_csv("test11.csv")
+#data = pd.read_csv("test11.csv")
+
+#print(A)
+
+data=pd.merge(A, on="user")
+column=['user', 'movieA', 'movieB', 'movieC', 'movieD', 'movieE']
+data=data[column]
+data
